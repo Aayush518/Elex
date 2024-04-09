7e551a3fd9d0f9bac374ee14f16d35870eee834d
Prayush Adhikari <115662438+adhikareeprayush@users.noreply.github.com>
2024-04-09 23:04:15 +0545

    Created a branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>One Fourty</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
-    <h1 class="text-3xl font-bold">
-        Hello world!
-    </h1>
+
 </body>
 
 </html>
